3f4f0a9e70aef2677fbf5bec6f097192e9c94336
Saeid Tafazzol <saeed.tafazzol@gmail.com>
2026-07-22 17:03:52 -0400

    moved into correct folders

--- dataset/dataset_gen_constrained.py
+++ dataset/dataset_gen_constrained.py
@@ -20,7 +20,7 @@ import sys
 from pathlib import Path
 _root = Path(__file__).resolve().parent.parent
 sys.path.insert(0, str(_root / "src"))
-sys.path.insert(1, str(_root / "experiments"))
+
 del _root
 
 
--- src/train.py
+++ src/train.py
@@ -3,7 +3,7 @@ import sys
 from pathlib import Path
 _root = Path(__file__).resolve().parent.parent
 sys.path.insert(0, str(_root / "src"))
-sys.path.insert(1, str(_root / "experiments"))
+sys.path.insert(1, str(_root / "dataset"))
 del _root
 import argparse
 import json
